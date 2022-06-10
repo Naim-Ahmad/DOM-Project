d90cc2a9d271690c29dd530d5dd54da015b99629
maliha <maliha@gmail.com>
2022-06-10 20:47:07 +0600

    I have complited two dom project

---
+++ project-2/app.js
@@ -0,0 +1,36 @@
+// Change the background color by cliking button
+// and show hax color on disabled input field
+
+// step-1 onload function
+
+window.onload = () => {
+        main()
+}
+    
+// step-2 main function
+
+function main() {
+    const root = document.getElementById('root')
+    const btn = document.getElementById('change-btn')
+    const output = document.getElementById('output')
+
+    btn.addEventListener('click', function () {
+        const bgColor = generateHaxColor()
+        root.style.backgroundColor = bgColor
+        output.value = bgColor
+    })
+}
+    
+// step-3 generate hax color
+
+function generateHaxColor() {
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
+    
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+}
+
+// step-4 collect all necessary information
+
+// step-5 click event handler
