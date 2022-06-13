8cb009c7aee6f88c0214e0da70665d081adacc0c
maliha <maliha@gmail.com>
2022-06-14 01:18:51 +0600

    fixed some bug 24%12 = 0

--- Clock/app.js
+++ Clock/app.js
@@ -2,21 +2,24 @@ const hours = document.getElementById("hour")
 const minute = document.getElementById("minute")
 const second = document.getElementById("second")
 const progress = document.getElementById("progress")
+const ampm = document.getElementById('ampm')
+
 
 function getCorrentTime() {
     let date = new Date()
     // date.toString()
-    console.log(date.toString())
     let hr = date.getHours()
-    console.log(hr)
     let min = date.getMinutes()
     let sec = date.getSeconds()
 
-    hours.textContent = hr % 12 < 10?('0'+hr): hr%12
-    minute.textContent = min <10? ('0'+ min): min
+    hours.textContent =
+        (hr % 12 < 10) ? (hr%12 == 0)? 12 : "0" + hr : hr
+    minute.textContent = min < 10 ? "0" + min : min
     second.textContent = sec < 10 ? "0" + sec : sec
 
     progress.style.width = (sec / 60) * 100 + "%"
+
+    ampm.textContent = (hr<=12)? 'AM': 'PM'
 }
 
 setInterval(getCorrentTime, 1000)
