5dd328f6b4bbc01c3612996ee8d19bd436616c81
sajid <mdbadimma24434@gmail.com>
2022-10-22 07:37:52 +0600

    i have done project 12 and the project was add custom color palet

--- test.js
+++ test.js
@@ -1,14 +1,47 @@
-let str = 'I love laptop'
+// let str = 'I love laptop'
 
-let replaced = str.replace(/laptop/gi, 'Allah')
+// let replaced = str.replace(/laptop/gi, 'Allah')
 
-console.log(replaced)
+// console.log(replaced)
 
-let rgbCode = 'rgb(60, 12, 36)'
+// let rgbCode = 'rgb(60, 12, 36)'
 
-let arrRgbCode = rgbCode.match(/\d/g)
+// let arrRgbCode = rgbCode.match(/\d/g)
 
-arrRgbCode.forEach(color => {
-    console.log(color)
-})
+// arrRgbCode.forEach(color => {
+//     console.log(color)
+// })
+
+// let arr = ['a', 'b', 'c', 'd']
+
+// let arr2 = arr.slice(0, 2)
+
+// console.log(arr2)
+
+// let arr = new Array(1)
+// console.log(arr)
+
+// let num = [1, 2, 3, 4, 5, 6]
+
+// num.push(8)
+
+// console.log(Array.isArray(num))
+
+// let num2 = [10, 11, 12]
+
+// let marged = num.concat(num2)
+
+// console.log(marged)
+
+let arr = [1, 2, 3, 4, 5, 6]
+arr.pop()
+console.log(arr)
+
+
+// let len = num.length
+
+// console.log(num[len - 1])
+// console.log(num[len - 3])
+// console.log(num[len - 4])
+// console.log(num[len - 5])
 
